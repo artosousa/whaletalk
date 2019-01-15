6398bf1c08c511d0c82763440bbd6e77e24c6406
Arthur Sousa <artosousa@gmail.com>
2019-01-15 16:16:02 -0500

    added text to speech feature

--- whaletalk.js
+++ whaletalk.js
@@ -2,6 +2,7 @@ const vowels = ['a','e','i','o','u'];
 const translated = document.getElementById('translated-box');
 const originalString = document.getElementById('original-string');
 const input = document.getElementById('user-input');
+const playWhaleSound = document.getElementById('play-sound');
 
 const translateToWhale = function(){
   let inputText = document.getElementById('user-input').value;
@@ -31,4 +32,9 @@ translated.innerHTML = resultArray.join("").toUpperCase();
 
 }
 
+const playSound = function(){
+  responsiveVoice.speak(translated.innerHTML.toLowerCase(), "Japanese Male", {pitch: 4});
+}
+
+playWhaleSound.addEventListener('mousedown', playSound);
 input.addEventListener('keyup', translateToWhale);
